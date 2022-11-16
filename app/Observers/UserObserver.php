b1b466291ef2188e523d6a8b7c634c8065954765
Richarles <richarles01@gmail.com>
2022-11-16 16:22:25 -0300

    first commit

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        Mail::send('emails',[ 'dados'=> $user],function ($message) use ($user){
+            $message->from('teste01@mail.com','Sistemas');
+            $message->to($user->email,'teste');
+            $message->subject('Email de teste enviado com sucesso');
+        });
+
+        echo 'Email enviado';
+    }
+
+    /**
+     * Handle the User "updated" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
